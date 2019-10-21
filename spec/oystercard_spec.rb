f0f1a9ff7c435ad64d6f79c55ea2775016eb7c95
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-21 13:55:52 +0100

    added subject

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,8 @@
+require "oystercard"
+
 describe Oystercard do
-    
+    subject {Oystercard.new}
+    it "should be 0 balance by default" do
+        expect(subject.balance).to eq 0 
+    end
 end
