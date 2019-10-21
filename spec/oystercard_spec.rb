b075bb676b9be85edd1654018f70687f21113000
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-21 14:08:40 +0100

    added test for top_up

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,4 +5,8 @@ describe Oystercard do
     it "should be 0 balance by default" do
         expect(subject.balance).to eq 0 
     end
+    it "checks if topup method is invoked" do 
+        subject.top_up(5)
+        expect(subject.balance).to eq 5
+    end
 end
