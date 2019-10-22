7790229191f49a7b0e1ef13cebec14c501515858
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-10-22 15:24:26 +0100

    added test for touch in

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,6 +21,8 @@ describe Oystercard do
         subject.top_up(10)
         expect{subject.deduct(11)}.to raise_error "the balance is below 0"
     end
-
-
+   it "should let you touch in" do
+    subject.touch_in
+    expect(subject.in_journey).to eq true
+   end
 end
